0744f0a72e39f31168aba912ac95ead8327aa0b1
jayson ambrose <jayson.ambrose.87@gmail.com>
2023-01-31 15:39:06 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+var allCats
+
+function destructivelyAppendCat(name){
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(name){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(name){
+    cats.shift();
+}
+
+function appendCat(name){
+    const allCats = [...cats, "Broom"];
+    return allCats
+}
+
+function prependCat(name){
+    const allCats = ["Arnold", ...cats];
+    return allCats
+}
+
+function removeLastCat(name){
+    
+    const allCats = cats.slice(0, -1);
+    return allCats;
+}
+
+function removeFirstCat(name){
+    
+    const allCats = cats.slice(1);
+    return allCats;
+}
+
+
+
